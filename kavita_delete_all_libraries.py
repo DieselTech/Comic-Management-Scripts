724575687d43748ed33843260fbec0832f026b9a
DieselTech <30128380+DieselTech@users.noreply.github.com>
2024-05-05 00:17:10 -0400

    Create kavita_delete_all_libraries.py
    Initial upload. 
    
    Made this because in my making of all the other scripts I've been doing I needed a way to clear out my libraries without starting kavita from scratch each time. This lets me keep my user and API key.

---
+++ kavita_delete_all_libraries.py
@@ -0,0 +1,106 @@
+"""
+Author: DieselTech
+URL: https://github.com/DieselTech/Comic-Management-Scripts
+Date created: May 4, 2024, 23:30 PM
+
+Description: This unbiasly deletes every library on the server. There is no customization of what it deletes. Just kills it all. 
+
+Software requirements:
+- Python 3
+- requests
+
+Usage:
+python kavita_delete_all_libraries.py
+
+     _.-^^---....,,--
+ _--                  --_
+<                        >)
+|                         |
+ \._                   _./
+    ```--. . , ; .--'''
+          | |   |
+       .-=||  | |=-.
+       `-=#$%&%$#=-'
+          | ;  :|
+ _____.,-#%&$@%#&#~,._____
+
+
+"""
+
+import requests
+from urllib.parse import urlparse
+import time
+import random
+
+def authenticate(url):
+    parsed_url = urlparse(url)
+    host_address = parsed_url.scheme + "://" + parsed_url.netloc
+
+    api_key = parsed_url.path.split('/')[-1]
+    login_endpoint = "/api/Plugin/authenticate"
+    try:
+        apikeylogin = requests.post(
+            host_address + login_endpoint + "?apiKey=" + api_key + "&pluginName=pyNuke")
+        apikeylogin.raise_for_status()
+        jwt_token = apikeylogin.json()['token']
+        return jwt_token, host_address
+    except requests.exceptions.RequestException as e:
+        print("Error during authentication:", e)
+        exit()
+
+
+def get_all_libraries(jwt_token, host_address):
+    get_all_libraries_endpoint = "/api/Library/libraries"
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+        "Content-Type": "application/json"
+    }
+    response = requests.get(host_address + get_all_libraries_endpoint, headers=headers)
+    if response.status_code != 200:
+        print("Error: Failed to get data from API.")
+        return
+    # take the response and store the library IDs in a list
+    library_ids = []
+    for library in response.json():
+        library_ids.append(library['id'])
+    return library_ids
+
+
+def delete_all_libraries(jwt_token, host_address):
+    delete_endpoint = "/api/Library/delete"
+    headers = {
+        "Authorization": f"Bearer {jwt_token}",
+        "Content-Type": "application/json"
+    }
+    # take the library IDs and delete them
+    library_ids = get_all_libraries(jwt_token, host_address)
+
+    for library_id in library_ids:
+        string_it = str(library_id)
+        build_url = host_address + delete_endpoint + "?libraryid=" + string_it
+        response = requests.delete(build_url, headers=headers)
+        if response.status_code != 200:
+            print("Error: Failed to delete data from API.")
+        else:
+            print(f"Library '{library_id}' deleted. Response: {response.status_code}")
+    return
+
+
+def main():
+    sure = input("Are you sure you want to delete all libraries? (yes/no): ")
+    if sure.lower() == "yes":
+        really_sure = input("Are you really sure? (yes/no): ")
+        if really_sure.lower() == "yes":
+            print("Take a few seconds to think about it before dropping to DEFCON 1")
+            time.sleep(random.randint(5, 10))
+            url = input("Enter the server URL you want to nuke the libraries from: ")
+            jwt_token, host_address = authenticate(url)
+            get_all_libraries(jwt_token, host_address)
+            delete_all_libraries(jwt_token, host_address)
+        else:
+            print("Back to DEFCON 5")
+            exit()
+
+
+if __name__ == "__main__":
+    main()
